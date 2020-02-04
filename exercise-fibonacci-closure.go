ee2d2fb6d2fecd786d79f47db4334dfe4e57aaf1
Valentin Lecoq-Vallon <vlecoq-v@student.42.fr>
2020-02-04 13:27:24 +0100

    testing the go formating and go packages on vscode

--- exercise-fibonacci-closure.go
+++ exercise-fibonacci-closure.go
@@ -1,9 +1,11 @@
+package main
+
 import "fmt"
 
 func fibonacci() func() int {
 	suite := 0
 	suite2 := 1
-	return func () int {
+	return func() int {
 		res := suite2 + suite
 		suite = suite2
 		suite2 = res
