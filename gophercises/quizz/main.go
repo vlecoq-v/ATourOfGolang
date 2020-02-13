7a5b842fb00a05485995e10678fc47ca1ef8d0e1
Valentin LECOQ-VALLON <vlecoq-v@student.42.fr>
2020-02-13 19:42:09 +0100

    added timer to wait for a given number of seconds

--- gophercises/quizz/main.go
+++ gophercises/quizz/main.go
@@ -4,9 +4,9 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
-	"math/rand"
 	"time"
 )
 
@@ -14,6 +14,7 @@ func main() {
 	//Binary argument and info
 	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
 	shuffleFlag := flag.Bool("shuffle", false, "put this flag if you want to shuffle the questions")
+	timerFlag := flag.Int("timer", 30, "basic timer is 30s, define a value for this flag to overwrite")
 	flag.Parse()
 
 	//File Opening
@@ -35,23 +36,33 @@ func main() {
 
 	//questions and answers from the problems slice
 	var rights int
-	questionAndAnswers(problems, &rights)
-	fmt.Printf("TOTAL IS \n%v / %v GOOD ANSWERS", rights, len(problems))
+	channel := make(chan int)
+	fmt.Printf("%v", channel)
+	go questionAndAnswers(problems, channel, &rights)
+	timer := time.NewTimer(time.Duration(*timerFlag) * time.Second)
+	select {
+	case rights := <-channel:
+		timer.Stop()
+		fmt.Printf("TOTAL IS \n%v / %v GOOD ANSWERS", rights, len(problems))
+	case <-timer.C:
+		fmt.Printf("\n\n---   time RUN OUT   ---\n")
+		exit(fmt.Sprintf("TOTAL IS \n%v / %v GOOD ANSWERS", rights, len(problems)))
+	}
 }
 
 func csvParser(r *csv.Reader) []problem {
-		lines, err := r.ReadAll()
-		if err != nil {
-			exit(fmt.Sprintf("reading error %v", err))
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit(fmt.Sprintf("reading error %v", err))
+	}
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: strings.TrimSpace(line[1]),
 		}
-		ret := make([]problem, len(lines))
-		for i, line := range lines {
-			ret[i] = problem {
-				q: line[0],
-				a: strings.TrimSpace(line[1]),
-			}
-		}
-		return ret
+	}
+	return ret
 }
 
 type problem struct {
@@ -59,17 +70,17 @@ type problem struct {
 	a string
 }
 
-func questionAndAnswers(problems []problem, rights *int) {
+func questionAndAnswers(problems []problem, channel chan int, rights *int) {
 	for i, problem := range problems {
-		fmt.Printf("%d %v = ", i + 1, problem.q)
+		fmt.Printf("%d %v = ", i+1, problem.q)
 		var userAnswer string
 		fmt.Scanf("%s", &userAnswer)
 		if userAnswer == problem.a {
 			*rights++
-		} 
 		}
+	}
+	channel <- 1
 }
-
 
 func introduction() {
 	fmt.Println("This is a math test quizz")
